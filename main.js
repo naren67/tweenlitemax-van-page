869c4b1df399668a5e420a3a8fc15dbbeed664c4
naren67 <naren678n@gmail.com>
2021-05-16 22:49:31 +0530

    tweenlite cdn

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const p = document.querySelector('p')
 
 const t1 = new TimelineMax()
 
-t1.fromTo(img,1, {height:'0%'}, {height:'70%'}, ease:power2.easeInOut)
+t1.fromTo(img,1, {height:'0%'}, {height:'70%', ease: Power2.easeInOut})
+.fromTo(img,1.2,{width: '100%'}, {width: '80%'})
 
-t1.fromTo(slider,1.5, {x:'-100%'}, {x:'0%'})
+t1.fromTo(slider,1, {x:'-100%'}, {x:'0%'}, '-=1')
 
-t1.fromTo(p,2,{opacity:0}, {opacity:1})
+t1.fromTo(p,2,{opacity:0}, {opacity:1}).fromTo(p,1,{y:'-50%'}, {y:'0%'}, '-=2')
